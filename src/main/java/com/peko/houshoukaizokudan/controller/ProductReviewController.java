af30ac9a530d4abcb522c1b45d78e03f78cc922e
MarkHungBuddha <1909kram@gmail.com>
2023-10-27 05:07:09 +0800

    231027 controller增加註解

---
+++ src/main/java/com/peko/houshoukaizokudan/controller/ProductReviewController.java
@@ -0,0 +1,16 @@
+package com.peko.houshoukaizokudan.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductReviewController {
+
+    //新增Review(要跟orderdetail有關)
+
+    //product review by productid by page(平均評價)
+
+    //product review by time by page
+
+
+}
---
+++ src/main/java/com/peko/houshoukaizokudan/controller/QandAController.java
@@ -0,0 +1,20 @@
+package com.peko.houshoukaizokudan.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QandAController {
+
+    //找出廠商id還沒回覆QandA bv Page
+
+    //session抓memeberid 傳入productid 新增Q
+
+    //廠商刪除問題
+
+    //編輯QandA
+
+    //列出QandA by 廠商id bypage
+
+    //列出qanda by productid bypage
+}
