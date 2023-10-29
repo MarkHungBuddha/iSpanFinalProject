4cd3f8dd17578a7ebd2d30c4388ab84e2e80102a
MarkHungBuddha <1909kram@gmail.com>
2023-10-30 00:28:58 +0800

    231030 漏網之魚

---
+++ src/main/java/com/peko/houshoukaizokudan/controller/PasswordResetController.java
@@ -0,0 +1,38 @@
+package com.peko.houshoukaizokudan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.peko.houshoukaizokudan.service.PasswordResetService;
+
+@RestController
+
+public class PasswordResetController {
+
+    @Autowired
+    private PasswordResetService passwordResetService;
+
+    @PostMapping("/public/api/request")
+    public String requestPasswordReset(@RequestParam String email) {
+        try {
+            passwordResetService.requestPasswordReset(email);
+            return "驗證信已寄出";
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/public/api/reset")
+    public String resetPassword(@RequestParam String email, @RequestParam String resetToken, @RequestParam String newPassword) {
+        try {
+            boolean success = passwordResetService.resetPassword(email, resetToken, newPassword);
+            if (success) {
+                return "密碼更改成功";
+            } else {
+                return "更改失敗，請檢查驗證碼與密碼格式";
+            }
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
